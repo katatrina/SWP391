1d6fb6147f2831f843ae48f48272aa7005e1a170
Truong-Nguyen <dangtruongnguyen2@gmail.com>
2023-09-21 21:43:08 +0700

    Service Page Basic, Edited Sign up

--- ui/static/js/signup.js
+++ ui/static/js/signup.js
@@ -1,5 +1,6 @@
 function showHideForms() {
     var roleSelect = document.getElementById("role");
+    console.log(roleSelect);
     var businessInfo = document.getElementById("businessInfo");
 
     if (roleSelect.value === "provider") {
