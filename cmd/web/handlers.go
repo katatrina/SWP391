45131ceee8c0d69badbb72c5c7339acde119bdee
chauvinhphuoc <cvphuoc2014@gmail.com>
2023-09-29 10:45:24 +0700

    add signup customer

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"errors"
-	"fmt"
 	"github.com/katatrina/SWP391/internal/db/sqlc"
 	"github.com/katatrina/SWP391/internal/validator"
 	"github.com/lib/pq"
@@ -42,28 +41,30 @@ func (app *application) about(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "about.html", data)
 }
 
-func (app *application) displayUserSignupPage(w http.ResponseWriter, r *http.Request) {
+func (app *application) displayMainSignupPage(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-	data.Form = userSignupFormResult{}
 
 	app.render(w, http.StatusOK, "signup.html", data)
 }
 
-type userSignupFormResult struct {
-	Role                string `form:"role"`
-	SelectedRole        string `form:"-"`
+type customerSignupFormResult struct {
 	FullName            string `form:"fullName"`
 	Email               string `form:"email"`
 	Phone               string `form:"phone"`
 	Address             string `form:"address"`
-	CompanyName         string `form:"companyName"`
-	TaxCode             string `form:"taxCode"`
 	Password            string `form:"password"`
 	validator.Validator `form:"-"`
 }
 
-func (app *application) doSignupUser(w http.ResponseWriter, r *http.Request) {
-	var form userSignupFormResult
+func (app *application) displaySignupCustomerPage(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = customerSignupFormResult{}
+
+	app.render(w, http.StatusOK, "signup_customer.html", data)
+}
+
+func (app *application) doSignupCustomer(w http.ResponseWriter, r *http.Request) {
+	var form customerSignupFormResult
 
 	err := app.decodePostForm(r, &form)
 	if err != nil {
@@ -79,134 +80,226 @@ func (app *application) doSignupUser(w http.ResponseWriter, r *http.Request) {
 		form.AddFieldError("phone", "This field must be a valid phone number")
 	}
 
-	// TODO: Validate address more detailed.
+	// TODO: Validate other fields more detailed.
 
-	if !validator.IsStringNotLessThanLimit(form.Password, 8) {
-		form.AddFieldError("password", "This field must be at least 8 characters long")
+	if !form.IsNoErrors() {
+		data := app.newTemplateData(r)
+		data.Form = form
+
+		app.render(w, http.StatusUnprocessableEntity, "signup_customer.html", data)
+		return
 	}
 
-	app.infoLog.Println(form.Role)
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-	// Register a Provider account.
-	if form.Role == "provider" {
-		form.SelectedRole = "provider"
-		app.infoLog.Println(form)
+	arg := sqlc.CreateCustomerParams{
+		FullName: form.FullName,
+		Email:    form.Email,
+		Phone:    form.Phone,
+		Address:  form.Address,
+		Password: string(hashedPassword),
+	}
 
-		if !validator.IsNotBlank(form.CompanyName) {
-			form.AddFieldError("companyName", "This field cannot be blank")
-		}
-
-		if !validator.IsNotBlank(form.TaxCode) {
-			form.AddFieldError("taxCode", "This field cannot be blank")
-		}
-
-		// TODO: Validate company name more detailed.
-
-		// TODO: Validate tax code more detailed.
-		fmt.Println("CCCCC")
-
-		if !form.IsNoErrors() {
-			data := app.newTemplateData(r)
-			data.Form = form
-			fmt.Println(form)
-
-			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
-			return
-		}
-
-		fmt.Println("WTF")
-		app.errorLog.Println(form)
-
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
-
-		err = app.store.CreateProviderTx(r.Context(), sqlc.CreateProviderTxParams{
-			FullName:    form.FullName,
-			Email:       form.Email,
-			Phone:       form.Phone,
-			Address:     form.Address,
-			CompanyName: form.CompanyName,
-			TaxCode:     form.TaxCode,
-			Password:    string(hashedPassword),
-		})
-		app.errorLog.Println(err)
-		if err != nil {
-			var postgreSQLError *pq.Error
-			if errors.As(err, &postgreSQLError) {
-				code := postgreSQLError.Code.Name()
-				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_email") {
-					form.AddFieldError("email", "Email address is already in use")
-				}
-
-				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_phone") {
-					form.AddFieldError("phone", "Phone number is already in use")
-				}
-
-				data := app.newTemplateData(r)
-				data.Form = form
-				app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
-				return
+	err = app.store.CreateCustomer(r.Context(), arg)
+	if err != nil {
+		var postgreSQLError *pq.Error
+		if errors.As(err, &postgreSQLError) {
+			code := postgreSQLError.Code.Name()
+			if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_email") {
+				form.AddFieldError("email", "Email address is already in use")
 			}
 
-			app.serverError(w, err)
-			return
-		}
-	} else {
-		form.SelectedRole = "customer"
-		// Register a Customer account.
-		if !form.IsNoErrors() {
+			if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_phone") {
+				form.AddFieldError("phone", "Phone number is already in use")
+			}
+
 			data := app.newTemplateData(r)
 			data.Form = form
 
-			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+			app.render(w, http.StatusUnprocessableEntity, "signup_customer.html", data)
 			return
 		}
 
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
-		if err != nil {
-			app.serverError(w, err)
-			return
-		}
-
-		arg := sqlc.CreateCustomerParams{
-			FullName: form.FullName,
-			Email:    form.Email,
-			Phone:    form.Phone,
-			Address:  form.Address,
-			Password: string(hashedPassword),
-		}
-
-		err = app.store.CreateCustomer(r.Context(), arg)
-		if err != nil {
-			var postgreSQLError *pq.Error
-			if errors.As(err, &postgreSQLError) {
-				code := postgreSQLError.Code.Name()
-				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_email") {
-					form.AddFieldError("email", "Email address is already in use")
-				}
-
-				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_phone") {
-					form.AddFieldError("phone", "Phone number is already in use")
-				}
-
-				data := app.newTemplateData(r)
-				data.Form = form
-				app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
-				return
-			}
-
-			app.serverError(w, err)
-			return
-		}
+		app.serverError(w, err)
+		return
 	}
 
 	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
 
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
 }
+
+type providerSignupFormResult struct {
+	FullName            string `form:"fullName"`
+	Email               string `form:"email"`
+	Phone               string `form:"phone"`
+	Address             string `form:"address"`
+	CompanyName         string `form:"companyName"`
+	TaxCode             string `form:"taxCode"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
+func (app *application) displaySignupProviderPage(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+	data.Form = providerSignupFormResult{}
+
+	app.render(w, http.StatusOK, "signup_provider.html", data)
+}
+
+func (app *application) doSignupProvider(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//func (app *application) doSignupUser(w http.ResponseWriter, r *http.Request) {
+//	var form customerSignupFormResult
+//
+//	err := app.decodePostForm(r, &form)
+//	if err != nil {
+//		app.clientError(w, http.StatusBadRequest)
+//		return
+//	}
+//
+//	if !validator.IsMatchRegex(form.Email, validator.EmailRX) {
+//		form.AddFieldError("email", "This field must be a valid email address")
+//	}
+//
+//	if !validator.IsMatchRegex(form.Phone, validator.PhoneRX) {
+//		form.AddFieldError("phone", "This field must be a valid phone number")
+//	}
+//
+//	// TODO: Validate address more detailed.
+//
+//	if !validator.IsStringNotLessThanLimit(form.Password, 8) {
+//		form.AddFieldError("password", "This field must be at least 8 characters long")
+//	}
+//
+//	app.infoLog.Println(form.Role)
+//
+//	// Register a Provider account.
+//	if form.Role == "provider" {
+//		form.SelectedRole = "provider"
+//		app.infoLog.Println(form)
+//
+//		if !validator.IsNotBlank(form.CompanyName) {
+//			form.AddFieldError("companyName", "This field cannot be blank")
+//		}
+//
+//		if !validator.IsNotBlank(form.TaxCode) {
+//			form.AddFieldError("taxCode", "This field cannot be blank")
+//		}
+//
+//		// TODO: Validate company name more detailed.
+//
+//		// TODO: Validate tax code more detailed.
+//		fmt.Println("CCCCC")
+//
+//		if !form.IsNoErrors() {
+//			data := app.newTemplateData(r)
+//			data.Form = form
+//			fmt.Println(form)
+//
+//			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+//			return
+//		}
+//
+//		fmt.Println("WTF")
+//		app.errorLog.Println(form)
+//
+//		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
+//		if err != nil {
+//			app.serverError(w, err)
+//			return
+//		}
+//
+//		err = app.store.CreateProviderTx(r.Context(), sqlc.CreateProviderTxParams{
+//			FullName:    form.FullName,
+//			Email:       form.Email,
+//			Phone:       form.Phone,
+//			Address:     form.Address,
+//			CompanyName: form.CompanyName,
+//			TaxCode:     form.TaxCode,
+//			Password:    string(hashedPassword),
+//		})
+//		app.errorLog.Println(err)
+//		if err != nil {
+//			var postgreSQLError *pq.Error
+//			if errors.As(err, &postgreSQLError) {
+//				code := postgreSQLError.Code.Name()
+//				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_email") {
+//					form.AddFieldError("email", "Email address is already in use")
+//				}
+//
+//				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_phone") {
+//					form.AddFieldError("phone", "Phone number is already in use")
+//				}
+//
+//				data := app.newTemplateData(r)
+//				data.Form = form
+//				app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+//				return
+//			}
+//
+//			app.serverError(w, err)
+//			return
+//		}
+//	} else {
+//		form.SelectedRole = "customer"
+//		// Register a Customer account.
+//		if !form.IsNoErrors() {
+//			data := app.newTemplateData(r)
+//			data.Form = form
+//
+//			app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+//			return
+//		}
+//
+//		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(form.Password), bcrypt.DefaultCost)
+//		if err != nil {
+//			app.serverError(w, err)
+//			return
+//		}
+//
+//		arg := sqlc.CreateCustomerParams{
+//			FullName: form.FullName,
+//			Email:    form.Email,
+//			Phone:    form.Phone,
+//			Address:  form.Address,
+//			Password: string(hashedPassword),
+//		}
+//
+//		err = app.store.CreateCustomer(r.Context(), arg)
+//		if err != nil {
+//			var postgreSQLError *pq.Error
+//			if errors.As(err, &postgreSQLError) {
+//				code := postgreSQLError.Code.Name()
+//				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_email") {
+//					form.AddFieldError("email", "Email address is already in use")
+//				}
+//
+//				if code == "unique_violation" && strings.Contains(postgreSQLError.Message, "users_uc_phone") {
+//					form.AddFieldError("phone", "Phone number is already in use")
+//				}
+//
+//				data := app.newTemplateData(r)
+//				data.Form = form
+//				app.render(w, http.StatusUnprocessableEntity, "signup.html", data)
+//				return
+//			}
+//
+//			app.serverError(w, err)
+//			return
+//		}
+//	}
+//
+//	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+//
+//	http.Redirect(w, r, "/login", http.StatusSeeOther)
+//}
 
 func (app *application) displayLoginPage(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -9,22 +9,31 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static/", fileServer))
+	fileServer1 := http.FileServer(http.Dir("./ui/static"))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static/", fileServer1))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, app.authenticate)
 
+	// TODO: Create a NotFound html file.
 	router.Handler(http.MethodGet, "/404", dynamic.ThenFunc(app.pageNotFound))
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
+
 	router.Handler(http.MethodGet, "/services", dynamic.ThenFunc(app.displayServicePage))
 	router.Handler(http.MethodGet, "/products", dynamic.ThenFunc(app.displayProductPage))
 	router.Handler(http.MethodGet, "/blogs", dynamic.ThenFunc(app.displayBlogPage))
-	router.Handler(http.MethodGet, "/signup", dynamic.ThenFunc(app.displayUserSignupPage))
-	router.Handler(http.MethodPost, "/signup", dynamic.ThenFunc(app.doSignupUser))
+	router.Handler(http.MethodGet, "/about", dynamic.ThenFunc(app.about))
+
+	router.Handler(http.MethodGet, "/signup", dynamic.ThenFunc(app.displayMainSignupPage))
+
+	router.Handler(http.MethodGet, "/signup/customer", dynamic.ThenFunc(app.displaySignupCustomerPage))
+	router.Handler(http.MethodPost, "/signup/customer", dynamic.ThenFunc(app.doSignupCustomer))
+
+	router.Handler(http.MethodGet, "/signup/provider", dynamic.ThenFunc(app.displaySignupProviderPage))
+	router.Handler(http.MethodPost, "/signup/provider", dynamic.ThenFunc(app.doSignupProvider))
+
 	router.Handler(http.MethodGet, "/login", dynamic.ThenFunc(app.displayUserLoginPage))
 	router.Handler(http.MethodPost, "/login", dynamic.ThenFunc(app.doLoginUser))
-	router.Handler(http.MethodGet, "/about", dynamic.ThenFunc(app.about))
 
 	protected := dynamic.Append(app.requireAuthentication)
 
